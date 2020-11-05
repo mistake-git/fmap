050aaa8b7f84345bbd90d1fef37171488f0422d9
mistake-git <acbmstk0402@gmail.com>
2020-11-05 19:37:44 +0900

    ボタンを実装しました

---
+++ front/src/components/posts/PostButtons.tsx
@@ -0,0 +1,42 @@
+import React, { Fragment, useContext, useEffect } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import Box from '@material-ui/core/Box';
+import EditIcon from '@material-ui/icons/Edit';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import IconButton from '@material-ui/core/IconButton';
+import { Link } from 'react-router-dom';
+import { NoEncryption } from '@material-ui/icons';
+
+const useStyles = makeStyles({
+	root: {
+		maxWidth: 290,
+	},
+	media: {
+		height: 160,
+	},
+	nounderline: {
+		textDecoration: 'none',
+	}
+});
+
+export default function PostCard() {
+  const classes = useStyles();
+
+	return (
+    <Fragment>
+      <Box mt={2}>
+        <Fab color="secondary">
+          <FavoriteIcon />
+        </Fab>
+      </Box>
+      <Box mt={2}>
+        <Fab color="primary">
+          <EditIcon/>
+        </Fab>
+      </Box>
+    </Fragment>
+	);
+}
--- front/src/pages/posts/PostsShow.tsx
+++ front/src/pages/posts/PostsShow.tsx
@@ -5,6 +5,12 @@ import auth from "../../firebase";
 import Template from "../../components/layouts/Template";
 import PostCard from "../../components/posts/PostCard"
 import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import Box from '@material-ui/core/Box';
+import EditIcon from '@material-ui/icons/Edit';
+import PostButtons from "../../components/posts/PostButtons"
 
 const useStyles = makeStyles((theme) => ({
   control: {
@@ -26,7 +32,11 @@ const PostsShow = (props: any) => {
       <Template>
         <Container maxWidth="lg">
           <Grid container style={{ marginTop: "1em" }}>
-            
+            <Grid item md={2} style={{ marginTop: "1em" }}>
+              <PostButtons/>
+            </Grid>
+            <Grid item md={8} style={{ marginTop: "1em" }}>メイン</Grid>
+            <Grid item md={2} style={{ marginTop: "1em" }}>ユーザーーカード</Grid>
           </Grid>
         </Container>
       </Template>
