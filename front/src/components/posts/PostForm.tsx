b06b3a517adedc7fe9842b99b982516580ac47a4
mistake-git <acbmstk0402@gmail.com>
2020-11-06 00:00:38 +0900

    投稿フォームを修正

--- front/src/components/posts/PostForm.tsx
+++ front/src/components/posts/PostForm.tsx
@@ -13,6 +13,8 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import Grid from '@material-ui/core/Grid';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 
 
 const useStyles = makeStyles({
@@ -51,81 +53,131 @@ const useStyles = makeStyles({
 		},
   ];
 
+
 export default function PostsForm() {
   const classes = useStyles();
+
+  const [state, setState] = React.useState({
+    private: true,
+    public: true,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
 	return (
 		<React.Fragment>
 			<Card className={classes.root}>
-      <CardContent>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            margin="dense"
-            id="name"
-            label="魚種"
-            type="text"
-            fullWidth
-            variant="outlined"
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                margin="dense"
+                id="name"
+                label="魚種"
+                type="text"
+                fullWidth
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+            <TextField
+                margin="dense"
+                id="number"
+                label="匹数"
+                type="number"
+                fullWidth
+                variant="outlined"
+              />        
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                id="size"
+                label="サイズ"
+                type="number"
+                fullWidth
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                margin="dense"
+                id="weight"
+                label="重さ"
+                type="number"
+                fullWidth
+                variant="outlined"
+              />        
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="outlined-select-currency-native"
+                select
+                label="天気"
+                margin="dense"
+                fullWidth
+                SelectProps={{
+                        native: true,
+                }}
+                variant="outlined"
+                >
+                {admins.map((admin) => (
+                  <option key={admin.value} value={admin.value}>
+                  {admin.label}
+                  </option>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="outlined-select-currency-native"
+                select
+                label="餌"
+                margin="dense"
+                fullWidth
+                SelectProps={{
+                  native: true,
+                }}
+                variant="outlined"
+                >
+                {departments.map((department) => (
+                  <option key={department.value} value={department.value}>
+                  {department.label}
+                  </option>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid item xs={12} sm={12}>
+              <TextField
+                margin="dense"
+                id="weight"
+                label="メモ"
+                type="text"
+                fullWidth
+                variant="outlined"
+              />        
+            </Grid>
+          </Grid>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={state.public}
+                onChange={handleChange}
+                name="public"
+                color="primary"
+              />
+            }
+            label="公開する"
           />
-
-          <TextField
-            required
-            margin="dense"
-            id="number"
-            label="匹数"
-            type="number"
-            fullWidth
-            variant="outlined"
-          />        
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="outlined-select-currency-native"
-            select
-            label="部署を選択"
-            margin="dense"
-            fullWidth
-            SelectProps={{
-              native: true,
-            }}
-            helperText="部署を選択してください"
-            variant="outlined"
-            >
-            {departments.map((department) => (
-              <option key={department.value} value={department.value}>
-              {department.label}
-              </option>
-            ))}
-          </TextField>
-        </Grid>
-      </Grid>
-       
-          <TextField
-            id="outlined-select-currency-native"
-            select
-            label="権限を選択"
-            margin="dense"
-            fullWidth
-            SelectProps={{
-                    native: true,
-            }}
-            helperText="権限を選択してください"
-            variant="outlined"
-            >
-            {admins.map((admin) => (
-              <option key={admin.value} value={admin.value}>
-              {admin.label}
-              </option>
-            ))}
-          </TextField>
         </CardContent>
         <CardActions>
         <Button
-        variant="contained"
-        color="primary"
-        fullWidth
-        >
+          variant="contained"
+          color="primary"
+          fullWidth
+          >
           釣果を登録
         </Button>
         </CardActions>
