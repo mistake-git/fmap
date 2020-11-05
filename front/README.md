7af26f6efb3747dfde603af15109a281004f1e3e
mistake-git <acbmstk0402@gmail.com>
2020-11-05 14:16:02 +0900

    first

---
+++ front/README.md
@@ -0,0 +1,25 @@
+This is sample project of user management by using the elements below.
+
+- Firebase Authentication
+- Typescript
+- React
+- React Hooks
+- Material UI
+- Formik
+- Yup
+
+## Demo
+
+![demo](https://github.com/motoitanigaki/typescript-react-hooks-firebase_authentication-sample/blob/master/demo/demo2.gif)
+
+## To run the project
+
+1. Create a new firebase authentication project and rewrite config in ./src/firebase.ts
+2. As this project is built with React, you can run the by the command below.
+
+```
+$ cd {to the directory where package.json is in}
+$ yarn start
+```
+
+You will be able to open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ front/src/pages/Home.tsx
@@ -0,0 +1,58 @@
+import React, { Fragment, useContext, useEffect } from "react";
+
+import { Button, Container, Grid, Typography } from "@material-ui/core";
+
+import { AuthContext } from "../Auth";
+import auth from "../firebase";
+
+const Home = (props: any) => {
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    // if not logged in, redirect to login page
+    currentUser === null && props.history.push("/login");
+  }, [currentUser]);
+
+  return (
+    <Fragment>
+      <Container>
+        <Grid container style={{ marginTop: "1em" }}>
+          <Grid item md={4}></Grid>
+          <Grid item md={4}>
+            <Typography>ログインしています</Typography>
+            <Typography
+              variant="caption"
+              style={{
+                paddingTop: "2em",
+                paddingBottom: "2em",
+                whiteSpace: "pre"
+              }}
+            >
+              {currentUser && JSON.stringify(currentUser, null, 4)}
+              {currentUser && currentUser.email}さんでログイン中
+            </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              onClick={async event => {
+                try {
+                  await auth.signOut();
+                  props.history.push("/login");
+                } catch (error) {
+                  alert(error.message);
+                }
+              }}
+              style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+            >
+              ログアウト
+            </Button>
+          </Grid>
+          <Grid item md={4}></Grid>
+        </Grid>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default Home;
