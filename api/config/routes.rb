e2ac6b52a9682e2e7c460f9cafbc516c82a5d563
mistake-git <acbmstk0402@gmail.com>
2020-11-06 14:31:47 +0900

    記事のapiを作成

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/create'
+  get 'posts/update'
+  get 'posts/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
