8248aa8e9e7b1b6cc9579753a941fd7c15bad0aa
mistake-git <acbmstk0402@gmail.com>
2020-11-06 18:26:06 +0900

    footerを修正

--- front/src/components/layouts/Footer.tsx
+++ front/src/components/layouts/Footer.tsx
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Masataka　Kitano
+        MasatakaKitano
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
