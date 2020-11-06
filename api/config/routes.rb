a753d32cb221545d7db5aa66be57ca21dd62d295
mistake-git <acbmstk0402@gmail.com>
2020-11-06 14:32:41 +0900

    ルーティングを修正

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/create'
-  get 'posts/update'
-  get 'posts/destroy'
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
