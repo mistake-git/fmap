77b28697ee9e3e27bdda84abd205f2403b2b8a08
mistake-git <acbmstk0402@gmail.com>
2020-11-06 14:42:40 +0900

    indexアクションを修正しました

--- api/app/controllers/posts_controller.rb
+++ api/app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :destroy]
 
   def index
-    posts = post.order(created_at: :desc)
+    posts = Post.all.order(created_at: :desc)
     render json: posts
   end
 
