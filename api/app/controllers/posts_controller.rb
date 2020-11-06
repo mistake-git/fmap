49caf1f82fb86b39a9381ed9f6cc94d2021f13f4
mistake-git <acbmstk0402@gmail.com>
2020-11-06 14:38:28 +0900

    postscontroller作成

--- api/app/controllers/posts_controller.rb
+++ api/app/controllers/posts_controller.rb
@@ -1,16 +1,43 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+
   def index
+    posts = post.order(created_at: :desc)
+    render json: posts
   end
 
   def show
+    render json: @post
   end
 
   def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: post.errors
+    end
   end
 
   def update
+    if @post.update(majocategory_params)
+      render json: @post 
+    else
+      render json:  @post.errors
+    end
   end
 
   def destroy
+    @post.destroy
+  end
+
+  private
+
+  def set_post
+    @post = post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:name, size, weather, weight, date, time, number, feed, memo, status)
   end
 end
