1dffc1c7d59152e5f902b3e9f62a69a73d35d9a8
mistake-git <acbmstk0402@gmail.com>
2020-11-11 20:02:58 +0900

    画像を追加しました

--- front/src/components/layouts/AuthTemplate.tsx
+++ front/src/components/layouts/AuthTemplate.tsx
@@ -13,6 +13,8 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { datePickerDefaultProps } from '@material-ui/pickers/constants/prop-types';
+import { url } from 'inspector';
+
 
 function Copyright() {
   return (
@@ -32,7 +34,6 @@ const useStyles = makeStyles((theme) => ({
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
@@ -80,3 +81,4 @@ export default function AuthTemplate(props: any) {
     </Grid>
   );
 }
+
