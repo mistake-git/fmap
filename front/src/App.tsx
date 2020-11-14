74720b597421a11f905d0403f531585a01fdac7c
mistake-git <acbmstk0402@gmail.com>
2020-11-14 20:57:10 +0900

    記事編集ページを作成

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -12,6 +12,7 @@ import PostsShow from "./pages/posts/PostsShow";
 import PostsNew from "./pages/posts/PostsNew";
 import Mypage from "./pages/users/MyPage";
 import Users from "./pages/users/Users";
+import PostsEdit from "./pages/posts/PostsEdit";
 
 const App: React.FC = () => {
   return (
@@ -23,8 +24,9 @@ const App: React.FC = () => {
           <Route exact path="/signin" component={SignIn} />
           <Route exact path="/users" component={Users} />
           <Route exact path="/posts" component={Posts} />
+          <Route exact path="/posts/:id/edit" component={PostsEdit} />
           <Route exact path="/posts/:id" component={PostsShow} />
-          <Route exact path="/posts_new" component={PostsNew} />
+          <Route exact path="/posts/new" component={PostsNew} />
           <Route exact path="/mypage/:id" component={Mypage} />
         </AuthProvider>
       </Switch>
---
+++ front/src/pages/posts/PostsEdit.tsx
@@ -0,0 +1,44 @@
+import React, { Fragment, useContext, useEffect } from "react";
+import { Button, Container, Grid, Typography } from "@material-ui/core";
+import { AuthContext } from "../../Auth";
+import auth from "../../plugins/firebase";
+import Template from "../../components/layouts/Template";
+import PostCard from "../../components/posts/PostCard"
+import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import Box from '@material-ui/core/Box';
+import EditIcon from '@material-ui/icons/Edit';
+import PostButtons from "../../components/posts/PostButtons"
+import PostForm from "../../components/posts/PostForm"
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+  control: {
+    padding: theme.spacing(1.5),
+  },
+}));
+
+const PostsEdit = (props: any) => {
+  const { currentUser } = useContext(AuthContext);
+  const classes = useStyles();
+
+  useEffect(() => {
+    // if not logged in, redirect to login page
+    currentUser === null && props.history.push("/signin");
+  }, [currentUser]);
+
+  return (
+    <Fragment>
+      <Template>
+        <Container maxWidth="md">
+          <PostForm />
+        </Container>
+      </Template>
+    </Fragment>
+  );
+};
+export default PostsEdit;
