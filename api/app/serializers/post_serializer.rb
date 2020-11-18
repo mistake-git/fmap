9a721315d4e92d8ec76ab097b9621b387db263c7
mistake-git <acbmstk0402@gmail.com>
2020-11-18 19:33:14 +0900

    Serializerを修正

--- api/app/serializers/post_serializer.rb
+++ api/app/serializers/post_serializer.rb
@@ -1,4 +1,4 @@
-class PostSerializer < ActiveModel::Serialize
+class PostSerializer < ActiveModel::Serializer
   attributes :id, :name, :size, :weather, :weight, :date, :time, :number, :feed, :memo, :status, :created_at, :updated_at
   has_many :comments, serializer: CommentSerializer do
     object.comments.order(id: :desc)
