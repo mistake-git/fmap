7a604e9e8e5cbf0ce78aac36492cfad5fa5cf099
mistake-git <acbmstk0402@gmail.com>
2020-11-18 19:30:55 +0900

    apiを修正

--- api/app/serializers/user_serializer.rb
+++ api/app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :introduction
-  has_many :posts, serializer: Postserializer do
+  has_many :posts, serializer: Post Serializer do
     object.posts.order(created_at: :desc)
   end
 end
