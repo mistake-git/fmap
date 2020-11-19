4e14741cf7eb6659905c39b6cdb9dab2454d0714
mistake-git <acbmstk0402@gmail.com>
2020-11-19 18:16:33 +0900

    Mapを修正

--- front/src/components/map/GoogleMap.tsx
+++ front/src/components/map/GoogleMap.tsx
@@ -1,41 +1,279 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import GoogleMapReact from 'google-map-react'
-
+import { useState } from 'react'
+import GoogleMapReact, { MapOptions, Maps } from 'google-map-react'
+interface PinProps {
+  lat: number,
+  lng: number,
+  name: string
+}
+const GoogleMap = () => {
+  const [currentKey, setCurrentKey] = useState(-1)
+  const pins: PinProps[] = [
+    {
+      lat: 43.0543412,
+      lng: 141.355018,
+      name: 'お姉さんレーベル'
+    },
+    {
+      lat: 43.0543451,
+      lng: 141.3528293,
+      name: '姉キャバ ジャックローズ'
+    }
+  ]
+  const apiLoaded = (map: any, maps: any, pins: any) => {
+  }
+  const changeBalloon = (key: string) => {
+    const keyNumber = Number(key)
+    if (currentKey === keyNumber) {
+      setCurrentKey(-1)
+    } else {
+      setCurrentKey(keyNumber)
+    }
+  }
+  const createMapOptions = (maps: Maps): MapOptions => {
+    return {
+      mapTypeControlOptions: {
+        position: maps.ControlPosition.TOP_RIGHT,
+      },
+      mapTypeControl: false,
+      zoomControl: false,
+      scaleControl: false,
+      streetViewControl: false,
+      fullscreenControl: false,
+      styles: [
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#e9e9e9',
+            },
+            {
+              lightness: 17,
+            },
+          ],
+        },
+        {
+          featureType: 'landscape',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#f5f5f5',
+            },
+            {
+              lightness: 20,
+            },
+          ],
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.fill',
+          stylers: [
+            {
+              color: '#ffffff',
+            },
+            {
+              lightness: 17,
+            },
+          ],
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [
+            {
+              color: '#ffffff',
+            },
+            {
+              lightness: 29,
+            },
+            {
+              weight: 0.2,
+            },
+          ],
+        },
+        {
+          featureType: 'road.arterial',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#ffffff',
+            },
+            {
+              lightness: 18,
+            },
+          ],
+        },
+        {
+          featureType: 'road.local',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#ffffff',
+            },
+            {
+              lightness: 16,
+            },
+          ],
+        },
+        {
+          featureType: 'poi',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#f5f5f5',
+            },
+            {
+              lightness: 21,
+            },
+          ],
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#dedede',
+            },
+            {
+              lightness: 21,
+            },
+          ],
+        },
+        {
+          elementType: 'labels.text.stroke',
+          stylers: [
+            {
+              visibility: 'on',
+            },
+            {
+              color: '#ffffff',
+            },
+            {
+              lightness: 16,
+            },
+          ],
+        },
+        {
+          elementType: 'labels.text.fill',
+          stylers: [
+            {
+              saturation: 36,
+            },
+            {
+              color: '#333333',
+            },
+            {
+              lightness: 40,
+            },
+          ],
+        },
+        {
+          elementType: 'labels.icon',
+          stylers: [
+            {
+              visibility: 'off',
+            },
+          ],
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [
+            {
+              color: '#f2f2f2',
+            },
+            {
+              lightness: 19,
+            },
+          ],
+        },
+        {
+          featureType: 'administrative',
+          elementType: 'geometry.fill',
+          stylers: [
+            {
+              color: '#fefefe',
+            },
+            {
+              lightness: 20,
+            },
+          ],
+        },
+        {
+          featureType: 'administrative',
+          elementType: 'geometry.stroke',
+          stylers: [
+            {
+              color: '#fefefe',
+            },
+            {
+              lightness: 17,
+            },
+            {
+              weight: 1.2,
+            },
+          ],
+        },
+      ],
+    }
+  }
+  return (
+    <Wrapper>
+      <GoogleMapWrapper>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: 'AIzaSyDWr8k_Rxo4UwSFde8mcgUiLs2BwXh3qCM'
+          }}
+          defaultCenter={{
+            lat: 43.0543451,
+            lng: 141.3528293
+          }}
+          defaultZoom={15}
+          options={createMapOptions}
+          onGoogleApiLoaded={({ map, maps }) => apiLoaded(map, maps, pins)}
+          onChildClick={(key: string) => changeBalloon(key)}
+        >
+        {
+          pins.map((
+            pin: {
+              lat: number,
+              lng: number,
+              name: string
+            },
+            index: number) => (
+            <Pin
+              lat={pin.lat}
+              lng={pin.lng}
+            >
+            {pin.name}
+            {
+              index === currentKey &&
+              'こんにちは'
+            }
+            </Pin>
+          ))
+        }
+        </GoogleMapReact>
+      </GoogleMapWrapper>
+    </Wrapper>
+  )
+}
 
 const Wrapper = styled.div`
   text-align: center;
 `
+
 const Pin = styled.div<{
   lat: number,
   lng: number
 }>`
 `
+
 const GoogleMapWrapper = styled.div`
   height: 100vh;
   width: 100%;
 `
-const GoogleMap = () => (
-  <Wrapper>
-    <GoogleMapWrapper>
-    <GoogleMapReact
-        bootstrapURLKeys={{
-          key: "AIzaSyDWr8k_Rxo4UwSFde8mcgUiLs2BwXh3qCM"
-        }}
-        defaultCenter={{
-          lat: 35.0582954,
-          lng: 135.3466919
-        }}
-        defaultZoom={12}
-      >
-        <Pin
-          lat={43.0582954}
-          lng={141.3466919}
-        >
-        </Pin>
-      </GoogleMapReact>
-    </GoogleMapWrapper>
-  </Wrapper>
-)
 
-export default GoogleMap;
+export default GoogleMap
