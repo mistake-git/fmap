a1c44baa80a47166e2f426d6dde588f00aad5244
mistake-git <acbmstk0402@gmail.com>
2020-11-20 16:49:11 +0900

    ヘッダーを修正

--- front/src/components/layouts/Header.tsx
+++ front/src/components/layouts/Header.tsx
@@ -6,23 +6,15 @@ import Typography from '@material-ui/core/Typography';
 import { IconButton } from '@material-ui/core';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import { Link } from 'react-router-dom';
-import Popover from '@material-ui/core/Popover';
-import Box from '@material-ui/core/Box';
-import Divider from '@material-ui/core/Divider';
 import { AccountCircle} from '@material-ui/icons';
 import Button from '@material-ui/core/Button';
 import auth from "../../plugins/firebase";
 import axios from 'axios'
+import PinDropIcon from '@material-ui/icons/PinDrop';
+import CreateIcon from '@material-ui/icons/Create';
+import GroupIcon from '@material-ui/icons/Group';
+import ViewModuleIcon from '@material-ui/icons/ViewModule';
 
-
-
-const sections = [
-  { title: '地図', url: '/' },
-	{ title: '釣果一覧', url: '/posts' },
-  { title: '釣り人一覧', url: '/users' },
-  { title: '釣果を投稿', url: '/posts/new' },
-  { title: 'ランキング', url: '/ranking' },
-];
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
@@ -36,40 +28,17 @@ const useStyles = makeStyles((theme) => ({
     textDecoration: 'none',
     color: 'white'
   },
-  toolbarLink: {
-    padding: theme.spacing(1),
-    flexShrink: 0,
-  },
   typography: {
     width: 500,
-  },
-  toolbarSecondary: {
-    justifyContent: 'space-around',
-    overflowX: 'auto',
-    backgroundColor: theme.palette.background.paper,
   },
   link: {
     color: 'white'
   },
-
-  small: {
-    width: theme.spacing(3.5),
-    height: theme.spacing(3.5),
-  },
-  sticky :{
-    position: 'sticky',
-    top: 0,
-    "z-index": 1,
-  },
-  popver: {
-    height: 500,
-  }
 }));
 
 export default function Header(props: any) {
   const classes = useStyles();
   const { title } = props;
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [user, setUser] = React.useState<any>('');
 
   useEffect(() => {
@@ -86,22 +55,6 @@ export default function Header(props: any) {
   }, []);
 
 
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-
-  const handleClick = (event: any) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
-
 return (
   <React.Fragment>
     <Toolbar className={classes.toolbar}>
@@ -114,40 +67,45 @@ return (
       >
         <Link to="/" className={classes.topLink}> {title}</Link>
       </Typography>
-      <Popover
-        className={classes.popver}
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
+      <Link to={'/'}>
+        <IconButton
+          className={classes.link}
         >
-        <div className={classes.sticky}>
-          <Typography align="center" className={classes.typography}>
-            <Box py={1}>お知らせ</Box>
-            <Divider/>
-          </Typography>
-        </div>
-        <Typography align="center">お知らせはありません</Typography>
-        </Popover>
-        <Link to={`/mypage/${user.uid}`}>
-          <IconButton
-           className={classes.link}
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-          >
-          <AccountCircle />
+          <PinDropIcon/>
         </IconButton>
-        </Link>
-        
+      </Link>
+      <Link to={'/posts'}>
+        <IconButton
+          className={classes.link}
+        >
+          <ViewModuleIcon/>
+        </IconButton>
+      </Link>
+      <Link to={'/posts/new'}>
+        <IconButton
+          className={classes.link}
+        >
+          <CreateIcon/>
+        </IconButton>
+      </Link>
+      <Link to={'/users'}>
+        <IconButton
+          className={classes.link}
+        >
+          <GroupIcon/>
+        </IconButton>
+      </Link>
+      <Link to={`/mypage/${user.uid}`}>
+        <IconButton
+          className={classes.link}
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+        >
+        <AccountCircle />
+      </IconButton>
+      </Link>
+       
         <IconButton>
           <ExitToAppIcon
             className={classes.link}
@@ -160,17 +118,6 @@ return (
             }}
           />
         </IconButton>
-    </Toolbar>
-    <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
-      {sections.map((section) => (
-        <Button
-          key={section.title}
-          href={section.url}
-          className={classes.toolbarLink}
-          >
-          {section.title}
-        </Button>
-      ))}
     </Toolbar>
   </React.Fragment>
   );
