eedf415e74b93a72f84eb88ca482ed9b1034843e
mistake-git <acbmstk0402@gmail.com>
2020-11-29 22:18:58 +0900

    オブジェクトの値をグルーピングする処理を作成

--- front/src/components/chart/HorizontalBarChart.tsx
+++ front/src/components/chart/HorizontalBarChart.tsx
@@ -10,10 +10,30 @@ export default function HorizontalChart(props: any){
     return <div></div>
   }
 
-  const chartVal: any = Object.values(props.data); // グラフデータ（描画するデータ）
+  const obj = props.data
+
+  //オブジェクトを範囲でグルーピング
+  const buildJudger = (s: any) => {
+    const opt = s.split("-");
+    if(opt.length !== 2 || (opt[0] === "" && opt[1] === "")) return () => false;
+    const [first, second] = opt.map(Number);
+    if(opt[0] === "") return (a: any) => a <= second;
+    if(opt[1] === "") return (a: any) => first <= a;
+    return (a: any) => first <= a && a <= second;
+  }
+
+  const pivots = ['1-10', '11-20', '21-30', '31-40', '41-50', '51-60', '61-70', '71-80', '81-90', '91-100', '100-',];
+  const result = pivots.reduce((a,c)=>{
+    const f = buildJudger(c);
+    const sum = Object.entries(obj).reduce((a,c)=> f(Number(c[0])) ? a+Number(c[1]) : a ,0);
+    return {...a,[c]:sum};
+  },{});
+
+
+  const chartVal: any = Object.values(result); // グラフデータ（描画するデータ）
  
   const data = {
-    labels:  Object.keys(props.data),
+    labels:  pivots,
     datasets: [{
       data: chartVal,
       backgroundColor: [
