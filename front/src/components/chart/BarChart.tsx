9c9d01e46911fa3033dbf7701c836b23eb114b77
mistake-git <acbmstk0402@gmail.com>
2020-11-30 11:09:21 +0900

    グラフの描画を修正

--- front/src/components/chart/BarChart.tsx
+++ front/src/components/chart/BarChart.tsx
@@ -9,35 +9,12 @@ export default function BarChart(props: any) {
     return <div></div>
    } 
 
-  const chartVal: any = Object.values(props.data); // グラフデータ（描画するデータ）
-
   const numbers: number[] = Array.from(new Array(12)).map((v,i)=> i + 1)
-
   const month = numbers.map(x => x + "月")
-
-
-  //1から12までの数字を出力
-  //1から12をdataのkeyと比べて一致すればdataのvalueを返して一致しなければ0を返す
-  //これを12回行う
-
-  const obj = props.data
-
-  console.log(obj)
-
-  const result: any = numbers.map(((number: any) => {
-    var index = 0
-    for (let i = 0; Object.keys(obj).length; i++ ){
-      if (number.toString() == Object.keys(obj)[index]){
-        return Object.values(props.data)[index]
-      } else {
-        return 0
-      }
-    }
-    index++
-  }))
-
-  console.log(result)
-
+  const obj = props.data;
+  const defaults ={1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,}
+  const result={...defaults,...obj}
+  const chartVal: any = Object.values(result); // グラフデータ（描画するデータ）
 
   const data = {
     labels: month,
--- front/src/components/chart/LineChart.tsx
+++ front/src/components/chart/LineChart.tsx
@@ -9,9 +9,16 @@ export default function LineChart(props: any){
     return <div></div>
   }
 
-  const chartVal: any = Object.values(props.data); // グラフデータ（描画するデータ）
-
-  const times = Object.keys(props.data).map(x => x + "時")
+  const obj = props.data
+  const numbers: number[] = Array.from(new Array(24)).map((v,i)=> i + 1)
+  const times = numbers.map(x => x + "時")
+  const defaults =
+  {
+    1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0,
+    13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0, 24:0
+  }
+  const result={...defaults,...obj}
+  const chartVal: any = Object.values(result);
 
   const data = {
     labels: times,
