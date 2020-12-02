c414e397deab18fca76d042579c64f0df5b896e4
mistake-git <acbmstk0402@gmail.com>
2020-12-02 18:11:48 +0900

    プロフィール画像編集の処理を修正

--- api/app/controllers/user_images_controller.rb
+++ api/app/controllers/user_images_controller.rb
@@ -2,7 +2,7 @@ class UserImagesController < ApplicationController
   
   def update
     @user = User.find_by(uid: params[:id])
-    @user.update(image: params[:user][:image])
+    @user.update(image: params[:image])
     render json: @user
   end
 
