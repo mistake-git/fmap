6fdb9ba073c16df503ff1821b29401c961a7191b
mistake-git <acbmstk0402@gmail.com>
2020-12-02 18:04:36 +0900

    シリアライザーを修正

--- api/app/controllers/user_images_controller.rb
+++ api/app/controllers/user_images_controller.rb
@@ -2,8 +2,8 @@ class UserImagesController < ApplicationController
   
   def update
     @user = User.find_by(uid: params[:id])
-    @user.update(image: params[:image])
+    @user.update(image: params[:user][:image])
     render json: @user
   end
-  
+
 end
