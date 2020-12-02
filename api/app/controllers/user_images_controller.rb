8534f27e53d081facfe39167c45c927b081d1140
mistake-git <acbmstk0402@gmail.com>
2020-12-02 15:48:21 +0900

    画像編集処理を修正

--- api/app/controllers/user_images_controller.rb
+++ api/app/controllers/user_images_controller.rb
@@ -2,13 +2,13 @@ class UserImagesController < ApplicationController
   
   def update
     @user = User.find_by(uid: params[:id])
-    pp "#{@user.name}:::::::::::::::::::::::::"
-    if params[:image]
-      @user.image.attach(params[:image])
+    pp "#{params[:user][:image]}:::::::::::::::::"
+    if params[:user][:image]
+      @user.image.attach(params[:user][:image])
       image = url_for(@user.image)
-    elsif params[:image]
+    elsif params[:user][:image]
       blob = ActiveStorage::Blob.create_after_upload!(
-        io: StringIO.new((Base64.decode64(params[:image].split(",")[1]))),
+        io: StringIO.new((Base64.decode64(params[:user][:image].split(",")[1]))),
         filename: "user.png",
         content_type: "image/png",
       )
@@ -17,7 +17,7 @@ class UserImagesController < ApplicationController
     else
       image = params[:image]
     end
-    if @user.image.attach(params[:image])
+    if @user.image.attach(params[:user][:image])
       render json: @user
     end
   end
