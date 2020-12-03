c95fb5e9f34ab90ef5676c0d37a96fcf02bbc053
mistake-git <acbmstk0402@gmail.com>
2020-12-03 14:57:39 +0900

    釣果を地図に一覧表示

--- front/src/pages/Map.tsx
+++ front/src/pages/Map.tsx
@@ -1,13 +1,48 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from "react";
 import Template from '../components/layouts/Template'
 import GoogleMap from '../components/map/GoogleMap'
+import PostModel from "../models/PostModel";
+import Loading from "../components/layouts/Loading";
+import { myHttpClient } from "../plugins/axios";
+
 
 const Home = (props: any) => {
+
+  const [posts, setPosts] = React.useState<PostModel[] | null>(null)
+  const [loading, setLoading] = React.useState(true);
+　
+  //非同期で投稿をAPIから取得
+  const getPosts = async() => {
+    try { 
+    await
+      myHttpClient.get('/posts')
+      .then((results) => {
+        console.log(results)
+        setPosts(results.data)
+      })
+    }
+    catch (error) {
+      alert(error.message);
+    }
+  　setLoading(false);
+  }
+
+  useEffect(() => {
+    getPosts();
+  },[setPosts]);
+
   return (
     <Fragment>
+    {loading &&
+      <Loading />
+     }
+    {posts &&
       <Template>
-        <GoogleMap />
+        <GoogleMap
+          posts={posts}
+        />
       </Template>
+    }
     </Fragment>
   )
 }
