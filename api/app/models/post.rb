2053b2f16f7c648a18df0d829dc5105cd4000236
mistake-git <acbmstk0402@gmail.com>
2020-12-03 13:42:22 +0900

    PostModelを修正

--- api/app/models/post.rb
+++ api/app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   validates :name, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
   belongs_to :user
   has_one_attached :image
   has_many :comments, dependent: :destroy
