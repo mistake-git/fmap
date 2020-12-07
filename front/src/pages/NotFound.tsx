5cb85e4401fad5a7846d78cd0eeedf02a19825ba
mistake-git <acbmstk0402@gmail.com>
2020-12-07 14:45:15 +0900

    ページが見つからない時のコンポーネントを修正

--- front/src/pages/NotFound.tsx
+++ front/src/pages/NotFound.tsx
@@ -1,21 +1,39 @@
 import React, { Fragment} from "react";
-import { Container, Grid, Typography } from "@material-ui/core";
+import { Box, Container, Grid, Typography } from "@material-ui/core";
 import Template from "../components/layouts/Template";
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  errorText :{
+    color: '#3f51b5;',
+  },
+  textCenter :{
+    textAlign: 'center'
+  }
+}));
 
 
 const NotFound = (props: any) => {
+  const classes = useStyles();
+
   return (
     <Fragment>
       <Template>
-        <Container maxWidth="md">
-          <Grid container spacing={2} style={{ marginTop: "4em" }}>
-            <Typography>404</Typography>
-            <Typography>お探しのページは見つかりませんでした</Typography>
-            <Link to={'/'}>
-              <Typography>Topに戻る</Typography>
-            </Link>
-          </Grid>
+        <Container maxWidth="md" className={classes.textCenter}>
+          <Box style={{marginTop: '11rem', marginBottom: '11rem'}}>
+            <Box mt={3}>
+              <Typography variant="h2" gutterBottom className={classes.errorText}>404</Typography>
+            </Box> 
+            <Box mb={3}>
+              <Typography>お探しのページは見つかりませんでした</Typography>
+            </Box>
+            <Box>
+              <Link to={'/'}>
+                <Typography>Topに戻る</Typography>
+              </Link>
+            </Box>
+          </Box>
         </Container>
         </Template>
     </Fragment>
