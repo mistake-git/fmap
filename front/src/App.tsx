ab4ac9b59fa6c75f9d212745c09e940e4717f643
mistake-git <acbmstk0402@gmail.com>
2020-12-07 16:46:53 +0900

    currentUserのproviderを作成

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { AuthProvider } from "./Auth";
+import {UserProvider} from "./User"
 import Map from "./pages/Map";
 import SignIn from "./pages/SignIn";
 import Signup from "./pages/Signup";
@@ -17,19 +18,21 @@ const App: React.FC = () => {
   return (
     <Router>
       <AuthProvider>
-        <Switch>
-        　<Route exact path="/" component={Map} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/signin" component={SignIn} />
-          <Route exact path="/users" component={Users} />
-          <Route exact path="/posts" component={Posts} />
-          <Route exact path="/posts/:id/edit" component={PostsEdit} />
-          <Route exact path="/mypages/:id" component={Mypage} />
+        <UserProvider>
           <Switch>
-            <Route exact path="/posts/new" component={PostsNew} />
-            <Route exact path="/posts/:id" component={PostsShow} />
+          　<Route exact path="/" component={Map} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/users" component={Users} />
+            <Route exact path="/posts" component={Posts} />
+            <Route exact path="/posts/:id/edit" component={PostsEdit} />
+            <Route exact path="/mypages/:id" component={Mypage} />
+            <Switch>
+              <Route exact path="/posts/new" component={PostsNew} />
+              <Route exact path="/posts/:id" component={PostsShow} />
+            </Switch>
           </Switch>
-        </Switch>
+        </UserProvider>
       </AuthProvider>
     </Router>
   );
---
+++ front/src/User.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, useEffect, useState } from "react";
+import auth from "./plugins/firebase";
+import UserModel from "./models/UserModel";
+import { myHttpClient } from "./plugins/axios";
+
+
+const UserContext = createContext<any | null>(null);
+
+const UserProvider = (props: any) => {
+  const [user, setUser] = useState<UserModel | null >(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user: any) => {
+      myHttpClient.get(`/users/${user?.uid}`)
+      .then((results) => {
+        console.log(results)
+        setUser(results.data.user)
+      })
+      .catch((data) =>{
+        console.log(data.user)
+      })
+    });
+  }, []);
+  
+  return (
+    <UserContext.Provider
+      value={{
+        user: user
+      }}
+    >
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserContext, UserProvider };
