1efa7bbfe4c0cc8aaa84b1b1d52dc76ce448d25d
mistake-git <acbmstk0402@gmail.com>
2020-12-10 14:00:00 +0900

    検索メソッドを作成

--- front/src/pages/posts/Posts.tsx
+++ front/src/pages/posts/Posts.tsx
@@ -35,6 +35,13 @@ const Posts = (props: any) => {
     getPosts();
   },[setPosts]);
 
+  // const filter = (event: any, posts: PostModel[]) => {
+  //   const updatePosts = posts.filter((post) => {
+  //     return post.name.search( event.target.value.toLowerCase()) !== -1;
+  //   })
+  //   setPosts(updatePosts)
+  // }
+
   return (
     <Fragment>
       {loading &&
@@ -43,14 +50,15 @@ const Posts = (props: any) => {
       {posts &&
       <Template>
         <Container maxWidth="md">
+          <input type="text" placeholder="search"/>
           <Grid container spacing={2} style={{ marginTop: "4em" }}>
-          {posts.map((post) => {
-            return(
-              <Grid item xs={12} sm={6} md={4}>
-                <PostCard post={post} key={post.id}/>
-              </Grid>
-            )
-          })}
+            {posts.map((post) => {
+              return(
+                <Grid item xs={12} sm={6} md={4}>
+                  <PostCard post={post} key={post.id}/>
+                </Grid>
+              )
+            })}
           </Grid>
         </Container>
         </Template>
