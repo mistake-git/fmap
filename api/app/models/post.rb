7915d8c22a37c29cbdafc14108da405ad75dfdce
mistake-git <acbmstk0402@gmail.com>
2020-12-10 09:51:26 +0900

    投稿のnameをカタカナでバリデーションするように修正

--- api/app/models/post.rb
+++ api/app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
   validates :latitude, presence: true
   validates :longitude, presence: true
   belongs_to :user
