63b3e94e53fa4b01562f31a63ff099b73f4e7111
mistake-git <acbmstk0402@gmail.com>
2020-12-12 16:50:07 +0900

    routeのurlを設定

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  include Rails.application.routes.url_helpers
   validates :name, presence: true
   validates :email, presence: true
   validates :uid, presence: true
@@ -7,4 +8,9 @@ class User < ApplicationRecord
   has_many :likes_posts, through: :likes, source: :post
   has_many :posts, dependent: :destroy
   has_one_attached :image
+
+  def image_url
+    image.attached? ?  url_for(image) : nil
+  end
+
 end
