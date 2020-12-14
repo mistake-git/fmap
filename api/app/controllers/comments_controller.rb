0cf424e47a650c3e8e9a8d8fbc7387ea4b7ea580
mistake-git <acbmstk0402@gmail.com>
2020-12-14 13:29:34 +0900

    idを許可するようにAPIを修正

--- api/app/controllers/comments_controller.rb
+++ api/app/controllers/comments_controller.rb
@@ -33,7 +33,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :user_id)
+    params.require(:comment).permit(:id ,:content, :user_id)
   end
 
   def set_post
