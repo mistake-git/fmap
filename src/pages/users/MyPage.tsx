4c133da9cdfee39d3217b8d82ccf8e45412ba021
mistake-git <acbmstk0402@gmail.com>
2021-02-04 23:45:50 +0900

    引数を修正

--- src/pages/users/MyPage.tsx
+++ src/pages/users/MyPage.tsx
@@ -192,7 +192,7 @@ const MyPage = (props: Props) => {
   const destroyRelationships = async(user_id: number, follow_id: number) => {
     try { 
     await
-    　 UsersRepository.destroyRelationships(user_id, follow_id)
+    　 UsersRepository.destroyRelationships()
       .then(() => {
         console.log('destroy relationships')
         const message = 'フォローを解除しました'
