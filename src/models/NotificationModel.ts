02289279104d59b3199889a5a7e665a448daf262
mistake-git <acbmstk0402@gmail.com>
2021-02-13 21:58:40 +0900

    [add]NotificationModelを作成

---
+++ src/models/NotificationModel.ts
@@ -0,0 +1,15 @@
+import CommentModel from "./CommentModel";
+import PostModel from "./PostModel";
+import UserModel from "./UserModel";
+
+interface NotificationModel {
+  id: number
+  created_at: Date
+  visited: UserModel
+  visitor: UserModel
+  action: string
+  comment: CommentModel
+  post: PostModel
+}
+
+export default NotificationModel
