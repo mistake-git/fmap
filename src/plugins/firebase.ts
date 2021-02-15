ab86b4d2c7302d57329fb495e629dd2a63926ba3
mistake-git <acbmstk0402@gmail.com>
2021-02-15 18:50:33 +0900

    firebaseのwarnningを削除

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -1,7 +1,8 @@
 import 'firebase/auth'
 import 'firebase/firebase-firestore'
+import firebase from "firebase/app"
 
-import firebase from 'firebase'
+
 const apiKey = process.env.REACT_APP_FIREBASE_KEY
 
 const config = {
