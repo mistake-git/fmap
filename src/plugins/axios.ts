2207e59c2c701124cd86ff27bc8026e690259edd
rorenge7 <rorenge7@gmail.com>
2021-02-21 15:35:38 +0900

    JWT認証（Firebase）を追加

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,6 +1,19 @@
 import axios from 'axios'
 const env = process.env
 
-export const myHttpClient = axios.create({
+const myHttpClient = axios.create({
   baseURL: env.REACT_APP_BASEURL,
 })
+
+myHttpClient.interceptors.request.use (
+  function (config) {
+    const token = localStorage.getItem('id-token')
+    if (token) config.headers.Authorization = `Bearer ${token}`
+    return config
+  },
+  function (error) {
+    return Promise.reject (error)
+  }
+);
+
+export { myHttpClient }
